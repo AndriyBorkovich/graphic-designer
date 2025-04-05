2e5ca75fb72a3931c7942137c9aca57da639af55
Andriy Borkovich <borkovich25andri@gmail.com>
2025-04-05 19:24:56 +0300

    fix: tooltip and toast providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-
 import { useState, useEffect } from "react";
 import { Routes, Route, useBeforeUnload, useLocation } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "./components/ui/toaster";
+import { Sonner } from "./components/ui/sonner";
 import Index from "./pages/Index";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -12,6 +12,7 @@ import ProjectsPage from "./pages/ProjectsPage";
 import DocumentationPage from "./pages/DocumentationPage";
 import { ConfirmDialog } from "./components/dialogs/ConfirmDialog";
 import { useToast } from "./hooks/use-toast";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 function App() {
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState<boolean>(false);
@@ -42,7 +43,8 @@ function App() {
     if (hasUnsavedChanges) {
       // This will trigger browser's native dialog, which we can't fully customize
       // But we will at least set a custom message
-      const message = "You have unsaved changes. If you leave, your changes will be lost.";
+      const message =
+        "You have unsaved changes. If you leave, your changes will be lost.";
       event.preventDefault();
       event.returnValue = message;
       return message;
@@ -51,23 +53,26 @@ function App() {
 
   // Reset unsaved changes when navigating away from editor
   useEffect(() => {
-    if (!location.pathname.includes('/editor')) {
+    if (!location.pathname.includes("/editor")) {
       setHasUnsavedChanges(false);
     }
   }, [location]);
 
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/editor/:projectId?" element={<EditorPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/documentation" element={<DocumentationPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/editor/:projectId?" element={<EditorPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/documentation" element={<DocumentationPage />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </TooltipProvider>
     </AuthProvider>
   );
 }
